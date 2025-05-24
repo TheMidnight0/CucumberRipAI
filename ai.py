5a3f57f6d7590929a9a19f42688d4668c957f556
MIDNIGHT <themidnight0v@gmail.com>
2025-05-24 16:26:06 +0300

    Поддержка группы изображений, изменение параметров бота

--- ai.py
+++ ai.py
@@ -3,8 +3,16 @@ import numpy as np
 import torch
 
 model = torch.hub.load('yolov5', 'custom', path="yolov5/runs/train/exp/weights/best.pt", source="local")
+model.conf = 0.3
+model.iou = 0.35
 
-def predict(image, conf_thres=0.25):
+def conf_model(conf_thres: float | None = None, iou_thres: float | None = None):
+    if conf_thres != None and conf_thres != model.conf:
+        model.conf = conf_thres
+    if iou_thres != None and iou_thres != model.iou:
+        model.iou = iou_thres
+
+def predict(image):
     # Конвертируем байты в numpy array
     np_img = np.frombuffer(image, np.uint8)
     # Декодируем изображение
@@ -12,19 +20,17 @@ def predict(image, conf_thres=0.25):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     
     # Выполняем инференс
-    results = model(img) # type: ignore
+    results = model(img, size=640) # type: ignore
     
     # Фильтруем по порогу уверенности
     results = results.pandas().xyxy[0]  # pandas DataFrame с результатами
     
     # Рисуем рамки и метки на исходном изображении (BGR)
     for _, row in results.iterrows():
-        if row['confidence'] < conf_thres:
-            continue
         x1, y1, x2, y2 = int(row['xmin']), int(row['ymin']), int(row['xmax']), int(row['ymax'])
         label = f"{row['name']} {row['confidence']:.2f}"
         cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        cv2.putText(img, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
+        cv2.putText(img, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
     
     # Кодируем обратно в байты JPEG
     success, encoded_img = cv2.imencode('.jpg', cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
-import os, asyncio
+import os, asyncio, re
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
-from aiogram.types import BufferedInputFile
-from ai import predict
+from aiogram.types import BufferedInputFile, InputMediaPhoto
+import ai
 
 API_TOKEN = os.getenv('BOT_TOKEN') or ""
 
@@ -11,18 +11,62 @@ dp = Dispatcher()
 
 @dp.message(Command(commands=['start', 'help']))
 async def send_welcome(message: types.Message):
-    await message.answer("Здравствуй, путник! Я – жнец огуречной судьбы, знающий о всех тонкостях работы с ними. Все просто, отправь мне изображение огурцов, и я укажу тебе их уровень зрелости...или съедобности.")
+    await message.answer("Здравствуй, путник! Я – жнец огуречной судьбы, знающий о всех тонкостях работы с ними. " \
+    "Все просто, отправь мне изображение огурцов, и я укажу тебе их уровень зрелости...или съедобности. " \
+    "Также ты в любой момент можешь изменить параметры предсказаний, просто отправь в чат:\n" \
+    "iou = 0.0-1.0 - это позволит мне убирать предсказания, которые перекрывают другие больше чем на iou %\n" \
+    "conf = 0.0-1.0 - это позволит мне убирать предсказания, в которых я уверен меньше чем на conf %")
+
+def parse_params(param_string):
+    if param_string == None: 
+        return None, None
+
+    # Инициализируем параметры по умолчанию
+    conf = None
+    iou = None
+
+    # Ищем значения параметров в строке
+    conf_match = re.search(r'conf=([\d.]+)', param_string)
+    iou_match = re.search(r'iou=([\d.]+)', param_string)
+
+    # Если найдено значение для conf, преобразуем его в float
+    if conf_match:
+        conf = float(conf_match.group(1))
+
+    # Если найдено значение для iou, преобразуем его в float
+    if iou_match:
+        iou = float(iou_match.group(1))
+
+    return conf, iou
 
 @dp.message()
 async def classify(message: types.Message):
-    if message.photo:
-        photo = message.photo[-1]
-        file = await bot.get_file(photo.file_id)
-        file_bytes = await bot.download_file(str(file.file_path))
+    if parse_params(message.text) != (None, None):
+        conf, iou = parse_params(message.text)
+        ai.conf_model(conf, iou)
+        if (iou != None): 
+            await message.answer("Изменен порог IOU на %f" %iou)
+        if (conf != None): 
+            await message.answer("Изменен порог уверенности на %f" %conf)
+    elif parse_params(message.caption) != (None, None):
+        conf, iou = parse_params(message.caption)
+        ai.conf_model(conf, iou)
+        if (iou != None): 
+            await message.answer("Изменен порог IOU на %f" %iou)
+        if (conf != None): 
+            await message.answer("Изменен порог уверенности на %f" %conf)
 
-        predicted = predict(file_bytes.read()) # type: ignore
-        input_file = BufferedInputFile(predicted, filename="image.jpg")
-        await message.answer_photo(input_file)
+    if message.photo:
+        media_group = []
+        for photo in message.photo[-int(len(message.photo) / 3):]:
+            file = await bot.get_file(photo.file_id)
+            file_bytes = await bot.download_file(str(file.file_path))
+
+            predicted = ai.predict(file_bytes.read()) # type: ignore
+            media_group.append(InputMediaPhoto(media=BufferedInputFile(predicted, filename="image.jpg")))
+
+        await message.answer_media_group(media=media_group)
+
 
 async def main():
     await dp.start_polling(bot)
